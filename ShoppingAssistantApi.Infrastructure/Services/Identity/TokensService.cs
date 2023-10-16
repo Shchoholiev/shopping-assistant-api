39bed12f30077ded23b537ea347fd7d471793631
shchoholiev <sh.codework@gmail.com>
2023-10-16 03:22:49 +0000

    SA-14 Updated User model structure and Services

--- ShoppingAssistantApi.Infrastructure/Services/Identity/TokensService.cs
+++ ShoppingAssistantApi.Infrastructure/Services/Identity/TokensService.cs
@@ -5,10 +5,7 @@ using System.Text;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using MongoDB.Bson;
-using ShoppingAssistantApi.Application.IRepositories;
 using ShoppingAssistantApi.Application.IServices.Identity;
-using ShoppingAssistantApi.Application.Models.Identity;
 
 namespace ShoppingAssistantApi.Infrastructure.Services.Identity;
 
@@ -16,48 +13,14 @@ public class TokensService : ITokensService
 {
     private readonly IConfiguration _configuration;
 
-    private readonly IUsersRepository _usersRepository;
-
     private readonly ILogger _logger;
 
-    public TokensService(IConfiguration configuration, IUsersRepository usersRepository,
-                            ILogger<TokensService> logger)
+    public TokensService(
+        IConfiguration configuration, 
+        ILogger<TokensService> logger)
     {
         this._configuration = configuration;
-        this._usersRepository = usersRepository;
         this._logger = logger;
-    }
-
-    public async Task<TokensModel> RefreshUserAsync(TokensModel tokensModel, CancellationToken cancellationToken)
-    {
-        var principal = this.GetPrincipalFromExpiredToken(tokensModel.AccessToken);
-
-        var userId = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-        if (!ObjectId.TryParse(userId, out var objectId))
-        {
-            throw new InvalidDataException("Provided id is invalid.");
-        }
-
-        var user = await this._usersRepository.GetUserAsync(objectId, cancellationToken);
-        if (user == null || user?.RefreshToken != tokensModel.RefreshToken
-            || user?.RefreshTokenExpiryDate <= DateTime.UtcNow)
-        {
-            throw new SecurityTokenExpiredException();
-        }
-
-        var newAccessToken = this.GenerateAccessToken(principal.Claims);
-        var newRefreshToken = this.GenerateRefreshToken();
-        user.RefreshToken = newRefreshToken;
-        user.RefreshTokenExpiryDate = DateTime.UtcNow.AddDays(30);
-        await this._usersRepository.UpdateUserAsync(user, cancellationToken);
-
-        this._logger.LogInformation($"Refreshed user tokens.");
-
-        return new TokensModel
-        {
-            AccessToken = newAccessToken,
-            RefreshToken = newRefreshToken
-        };
     }
 
     public string GenerateAccessToken(IEnumerable<Claim> claims)
@@ -73,18 +36,16 @@ public class TokensService : ITokensService
     public string GenerateRefreshToken()
     {
         var randomNumber = new byte[32];
-        using (var rng = RandomNumberGenerator.Create())
-        {
-            rng.GetBytes(randomNumber);
-            var refreshToken = Convert.ToBase64String(randomNumber);
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        var refreshToken = Convert.ToBase64String(randomNumber);
 
-            this._logger.LogInformation($"Generated new refresh token.");
+        this._logger.LogInformation($"Generated new refresh token.");
 
-            return refreshToken;
-        }
+        return refreshToken;
     }
 
-    private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
     {
         var tokenValidationParameters = new TokenValidationParameters
         {
@@ -96,11 +57,10 @@ public class TokensService : ITokensService
             ValidateLifetime = false
         };
         var tokenHandler = new JwtSecurityTokenHandler();
-        SecurityToken securityToken;
-        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
         var jwtSecurityToken = securityToken as JwtSecurityToken;
-        if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
-                                        StringComparison.InvariantCultureIgnoreCase))
+        if (jwtSecurityToken == null 
+            || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
             throw new SecurityTokenException("Invalid token");
 
         this._logger.LogInformation($"Returned data from expired access token.");
@@ -117,7 +77,7 @@ public class TokensService : ITokensService
         var tokenOptions = new JwtSecurityToken(
             issuer: _configuration.GetValue<string>("JsonWebTokenKeys:ValidIssuer"),
             audience: _configuration.GetValue<string>("JsonWebTokenKeys:ValidAudience"),
-            expires: DateTime.UtcNow.AddMinutes(5),
+            expires: DateTime.UtcNow.AddMinutes(15),
             claims: claims,
             signingCredentials: signinCredentials
         );
