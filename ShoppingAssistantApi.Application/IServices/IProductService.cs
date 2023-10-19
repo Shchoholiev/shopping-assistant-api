504e67546665ecef35775d33d7e71bd25862550f
stasex <stasek.molchan@gmail.com>
2023-10-19 14:13:57 +0300

    added file for services

---
+++ ShoppingAssistantApi.Application/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using ShoppingAssistantApi.Application.Models.OpenAi;
+using ShoppingAssistantApi.Domain.Entities;
+
+namespace ShoppingAssistantApi.Application.IServices;
+
+public interface IProductService
+{
+    Task<List<string>> StartNewSearchAndReturnWishlist(Message message, CancellationToken cancellationToken);
+    
+    Task<List<string>> GetProductFromSearch(Message message, CancellationToken cancellationToken);
+}
---
+++ ShoppingAssistantApi.Infrastructure/Services/ProductServices.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using ShoppingAssistantApi.Application.IRepositories;
+using ShoppingAssistantApi.Application.IServices;
+using ShoppingAssistantApi.Domain.Entities;
+
+namespace ShoppingAssistantApi.Infrastructure.Services;
+
+public class ProductServices
+{
+    /*private readonly IWishlistsRepository _wishlistsRepository;
+    
+    private readonly IOpenAiService _openAiService;
+    private readonly IProductService _productService;
+
+    public ProductServices(IOpenAiService openAiService, IProductService productService)
+    {
+        _openAiService = openAiService;
+        _productService = productService;
+    }
+    
+    public async Task<Collection<string>> StartNewSearchAndReturnWishlist(Message message,
+        CancellationToken cancellationToken)
+    {
+        return null;
+    }
+
+    public async Task<Collection<string>> GetProductFromSearch(Message message, CancellationToken cancellationToken)
+    {
+        return null;
+    }*/
+}
